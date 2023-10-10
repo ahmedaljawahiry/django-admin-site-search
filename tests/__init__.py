e1dfdfff7cdb7488c2b38af725b28800b258981b
Ahmed Al-Jawahiry <ahmedaljawahiry@gmail.com>
2023-10-10 19:30:03 +0100

    rename search path to "site-search"

--- tests/__init__.py
+++ tests/__init__.py
@@ -3,8 +3,8 @@ from django.urls import reverse
 
 
 def request_search(client: Client, query: str = ""):
-    """Returns the response after performing a GET request against the "admin:search"
+    """Returns the response after performing a GET request against the "admin:site-search"
     endpoint, with the given client and query"""
-    url = f'{reverse("admin:search")}?q={query}'
+    url = f'{reverse("admin:site-search")}?q={query}'
 
     return client.get(url)
